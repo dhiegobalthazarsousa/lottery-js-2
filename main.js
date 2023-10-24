2e740ba100a4215c9dd2e4490ca7fbcd4b0048a2
Dhiego Henrique Balthazar de Sousa <dhiegobalthazar@gmail.com>
2023-10-24 16:05:14 -0300

    Adiciona os números no painel de números disponíveis

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-const h1 = document.querySelector("h1");
-h1.innerText="Hello World!";
-console.log(h1.innerText);
+const numbersContainer = document.querySelector("#numbers");
+
+const createCircle = (number) => {
+    let circle = document.createElement("div");
+    circle.setAttribute("class", "numberCircle");
+    circle.innerText = number;
+    return circle;
+}
+
+const fillNumbers = () => {
+    let initialValue = 1;
+    let interval = setInterval(()=>{
+        let circle = createCircle(initialValue++);
+        numbersContainer.appendChild(circle);
+        if(initialValue === 16){
+            clearInterval(interval);
+        }
+    },300)
+}
+
+window.onload = () => {
+    fillNumbers();
+}
